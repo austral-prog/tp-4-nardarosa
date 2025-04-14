9ab416f0fce6146218cd26f75c786e99257679b0
nardarosa <nardarosa2050@gmail.com>
2025-04-14 11:29:42 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,2 +1,23 @@
 def line():
-    print("TO DO")
+    coeficiente_A= float(input("Ingrese el coeficiente A: "))
+    coeficiente_B= float(input("Ingrese el coeficiente B: "))
+    coeficiente_X1= float(input("Ingrese el coeficiente X1: "))
+    coeficiente_X2= float(input("Ingrese el coeficiente X2: "))
+    print("El coeficiente A de su ecuación de la recta es:", coeficiente_A)
+    print("El coeficiente B de su ecuación de la recta es:", coeficiente_B)
+    print("El coeficiente X1 de su ecuación de la recta es:", coeficiente_X1)
+    print("El coeficiente X2 de su ecuación de la recta es:", coeficiente_X2)
+    print()    
+    print("Para la siguiente ecuación:")
+    print(f"\tY = {coeficiente_A}X + {coeficiente_B}")
+    print()  
+    print("Dados los siguientes puntos:")
+    print(f"\tP1 ({coeficiente_X1}, {(coeficiente_A*coeficiente_X1) + coeficiente_B})")
+    print(f"\tP2 ({coeficiente_X2}, {(coeficiente_A*coeficiente_X2) + coeficiente_B})")
+    print()    
+    Y1 = coeficiente_A * coeficiente_X1 + coeficiente_B
+    Y2 = coeficiente_A * coeficiente_X2 + coeficiente_B
+    distancia = math.sqrt((coeficiente_X2 - coeficiente_X1)**2 + (Y2 - Y1)**2)
+    print(f"La distancia entre ellos es: {distancia}")
+if __name__ == '__main__':
+    line()
